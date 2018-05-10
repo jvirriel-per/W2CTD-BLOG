50600340f70fa378be8349d483f15cdf1303f778
José Virriel <38949440+jvirriel-per@users.noreply.github.com>
2018-05-10 16:49:16 -0400

    changes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_objetc_or_404
 from django.utils import timezone
 from .models import Post
 
-def post_list(request):
+def post_list(request, pk):
 	posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request):
+	post = get_objetc_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
+
+
