a23a9c9595c7cd2d308e0f3dbbdea0adc37bd03f
José Virriel <38949440+jvirriel-per@users.noreply.github.com>
2018-05-10 16:54:23 -0400

    changes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_objetc_or_404
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,7 +7,7 @@ def post_list(request, pk):
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request):
-	post = get_objetc_or_404(Post, pk=pk)
+	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
 
