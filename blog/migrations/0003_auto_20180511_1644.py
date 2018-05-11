e58c22910b5e86849685637cc193a83c74b59e9f
José Virriel <38949440+jvirriel-per@users.noreply.github.com>
2018-05-11 16:58:12 -0400

    changes

---
+++ blog/migrations/0003_auto_20180511_1644.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_comment'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='comment',
+            old_name='author',
+            new_name='autor',
+        ),
+        migrations.RenameField(
+            model_name='comment',
+            old_name='text',
+            new_name='texto',
+        ),
+    ]
